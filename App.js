ba662314f4348146e505e20c1cbb2ce59f92edf9
vr1 <hivr1eva@gmail.com>
2024-02-24 21:33:33 -0500

    add sticker panning

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import ImageViewer from './components/ImageViewer';
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from "./components/EmojiList"
 import EmojiSticker from './components/EmojiSticker';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -47,7 +48,7 @@ export default function App() {
     }
   }
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -70,7 +71,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
